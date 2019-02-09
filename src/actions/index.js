0d473ed03739bfa1bc99877504113c839b9b4674
extended <ts@imagiq.jp>
2019-02-09 22:10:02 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+const INCREMENT = 'INCREMENT'
+const DECREMRNT = 'DECREMRNT'
+
+export const increment = () =>({
+    type: 'INCREMENT'
+})
+
+export const decrement = () =>({
+    type: 'DECREMRNT'
+})
